ddd9e1bde2c75bfea6fc17e4991ea419da75662a
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2025-06-28 14:59:14 +0300

    Setup internalization

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import './globals.scss';
+
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+};
+
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
