b9ead096253014dfabe1b59b2eeb146c24645e65
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2025-07-20 12:08:29 +0300

    Added Meta data and inject Yandex metrics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 import RocketChat from "@/components/rocket-chat/RocketChat";
 import { routing } from "@/i18n/routing";
 import Header from "@/components/header/Header";
@@ -16,10 +18,46 @@ const roboto = Roboto({
   variable: "--font-roboto",
 });
 
-export const metadata: Metadata = {
-  title: "",
-  description: "",
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "SEO" });
+  return {
+    title: "Ventimax",
+    description: t("metaData.description"),
+    metadataBase: new URL("https://ventimax.ru"),
+    alternates: {
+      canonical: "/",
+      languages: {
+        en: "https://ventimax.ru/en",
+        ru: "https://ventimax.ru/ru",
+        zh: "https://ventimax.ru/zh",
+      },
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      googleBot: {
+        index: true,
+        follow: true,
+        noimageindex: false,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    verification: {
+      google: "google",
+      yandex: "yandex",
+      yahoo: "yahoo",
+    },
+    category: "Livestocks Equipments",
+  };
+}
 
 export default async function RootLayout({
   children,
@@ -45,6 +83,24 @@ export default async function RootLayout({
             <Footer />
           </ModalToggleProvider>
         </NextIntlClientProvider>
+        <Script
+          id="yandex-metrica"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();
+       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+       ym(103392832, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+       });
+            `,
+          }}
+        />
       </body>
     </html>
   );
