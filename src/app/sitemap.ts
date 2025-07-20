96c8ac4b6f6d209a0c95aaffaf14841a3bc39a61
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2025-07-20 13:44:07 +0300

    Added robot.ts and sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,39 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ventimax.ru",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: "https://ventimax.ru/en",
+          ru: "https://ventimax.ru/ru",
+          zh: "https://ventimax.ru/zh",
+        },
+      },
+    },
+    {
+      url: "https://ventimax.ru/about",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: "https://ventimax.ru/en/about",
+          ru: "https://ventimax.ru/ru/about",
+          zh: "https://ventimax.ru/zh/about",
+        },
+      },
+    },
+    {
+      url: "https://ventimax.ru/products",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: "https://ventimax.ru/en/products",
+          ru: "https://ventimax.ru/ru/products",
+          zh: "https://ventimax.ru/zh/products",
+        },
+      },
+    },
+  ];
+}
