7ee41a9b3f498c28aed4f753dfe3b932aedf0e44
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2026-03-05 14:34:56 +0300

    Update: change contact ID identifier

--- src/components/contact-us-block/ContactUsBlock.tsx
+++ src/components/contact-us-block/ContactUsBlock.tsx
@@ -4,17 +4,17 @@ import "./contact-us-block.scss";
 import Form from "../form/Form";
 
 function ContactUsBlock() {
-  const t = useTranslations("Pages.home.contactBlock");
-  return (
-    <section className="contact-us" id="contactBlock">
-      <div className="contact-us__inner-wrapper">
-        <h3 className="contact-us__title">{t("title")}</h3>
-        <div className="contact-us__form">
-          <Form />
-        </div>
-      </div>
-    </section>
-  );
+   const t = useTranslations("Pages.home.contactBlock");
+   return (
+      <section className="contact-us" id="contact">
+         <div className="contact-us__inner-wrapper">
+            <h3 className="contact-us__title">{t("title")}</h3>
+            <div className="contact-us__form">
+               <Form />
+            </div>
+         </div>
+      </section>
+   );
 }
 
 export default ContactUsBlock;
